54302b0c616b7a4d6a0fa35e8471db1736a82380
DominikDotzauer <36645002+DominikDotzauer@users.noreply.github.com>
2020-01-31 22:39:47 +0100

    New: Waypointsystem || Updated: n/a || Fixed: n/a

---
+++ RepairDefense/Assets/_Scripts/_Waypointsystem/Waypoint.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> waypoints;
+    [SerializeField][Range(0, 5)]
+    private float waypointsize;
+    [SerializeField]
+    private GameObject currentWaypoint;
+    [SerializeField]
+    Color gizmoColor;
+
+    private void Awake()
+    {
+        if (waypoints[0] != null) 
+        { 
+            currentWaypoint = waypoints[0];
+        }
+    }
+    public GameObject GetCurrentWaypoint()
+    {
+        return currentWaypoint;
+    }
+
+    public List<GameObject> GetWaypoints()
+    {
+        return waypoints;
+    }
+
+    public void AddWayPoint(GameObject waypoint)
+    {
+        waypoints.Add(waypoint);
+    }
+
+    public void SetCurrentWaypoint(int index)
+    {
+        if(waypoints[index] != null)
+        {
+            currentWaypoint = waypoints[index];
+        }
+        else
+        {
+            Debug.LogError("Waypoint not available! Waypointsize: " + waypoints.Count + "||" + "Wanted index: " + index);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        foreach (var waypoint in waypoints)
+        {
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawWireSphere(waypoint.transform.position, waypointsize);
+        }
+    }
+}
---
+++ RepairDefense/Assets/_Scripts/_Waypointsystem/WaypointMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Waypoint waypoint;
+    [SerializeField]
+    private bool moveToWaypoint;
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private int waypointIndex;
+
+    public WaypointMovement()
+    {
+        moveToWaypoint = false;
+        waypointIndex = 0;
+    }
+
+    public void SetMove(bool moveToWaypoint)
+    {
+        this.moveToWaypoint = moveToWaypoint;
+    }
+    private void Move()
+    {
+        float distance = movementSpeed * Time.deltaTime;
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, waypoint.GetCurrentWaypoint().transform.position, distance);
+        Debug.Log(distance);
+    }
+
+    public void SetNextMovement()
+    {
+        waypointIndex++;
+        waypoint.SetCurrentWaypoint(waypointIndex);
+    }
+    private void Awake()
+    {
+        this.gameObject.GetComponent<Waypoint>();
+    }
+
+    private void Update()
+    {
+        if (moveToWaypoint)
+        {
+            Move();
+        }
+    }
+}
