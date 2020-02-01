2a5a70cd3f52b4488080cec24757c18b42464bc1
Marcel Behrmann <mail@marcelbehrmann.de>
2020-02-01 02:08:24 +0100

    Changed a lot of stuff
    * Added Nature Pack
    * Implemented Base Spawning Version
    * Implemented Base Grid

---
+++ RepairDefense/Assets/_Scripts/WaveManagement/Asteroid.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10f;
+
+    Vector3 target;
+
+    Dictionary<GameObject, int> payload = new Dictionary<GameObject, int>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPos = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = newPos;
+    }
+
+    public void SetTarget(Vector3 pos)
+    {
+        target = pos;
+    }
+
+    public void AddToPayload(GameObject enemy, int count)
+    {
+        this.payload.Add(enemy, count);
+    }
+
+    public Dictionary<GameObject, int> GetPayload()
+    {
+        return payload;
+    }
+}
---
+++ RepairDefense/Assets/_Scripts/WaveManagement/SpawnArea.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 size = new Vector3(1, 0.1f, 1);
+
+    [SerializeField]
+    public bool active = false;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        var asteroid = collider.GetComponentInParent<Asteroid>();
+
+        if (asteroid == null)
+        {
+            return;
+        }
+
+        // TODO: Play Explosion
+
+        var payload = asteroid.GetPayload();
+
+        foreach(KeyValuePair<GameObject, int> spawn in payload)
+        {
+            for (var i = 0; i < spawn.Value; i++)
+            {
+                Instantiate(spawn.Key, transform.position, Quaternion.identity);
+            }
+        }
+
+        Destroy(asteroid.gameObject);
+    }
+}
