dc334ae57b79ed2dfe9ae4621d71480a1a63bc4b
Viktor Wolf <mail@scatteredwolf.com>
2020-02-01 10:35:41 +0100

    Firing Event on interaction
    Interaction.cs -> unityEventInteraction

--- RepairDefense/Assets/_Scripts/Player/Interaction.cs
+++ RepairDefense/Assets/_Scripts/Player/Interaction.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Interaction : MonoBehaviour
 {
+    UnityEvent unityEventInteraction;
+    private void Start()
+    {
+        if (unityEventInteraction == null)
+        {
+            unityEventInteraction = new UnityEvent();
+        }
+    }
     void InteractionEvent()
     {
-        Debug.Log("Interaction");
+        unityEventInteraction.Invoke();
     }
 }
