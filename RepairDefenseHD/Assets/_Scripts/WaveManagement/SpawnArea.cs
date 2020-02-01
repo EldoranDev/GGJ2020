20c2ea40b1c3c9c5595b3f3a9febc24047eaa5f6
Marcel Behrmann <info@eldoan.dev>
2020-02-02 00:58:28 +0100

    Create better looking spawn asteroids

--- RepairDefenseHD/Assets/_Scripts/WaveManagement/SpawnArea.cs
+++ RepairDefenseHD/Assets/_Scripts/WaveManagement/SpawnArea.cs
@@ -47,23 +47,30 @@ public class SpawnArea : MonoBehaviour
 
         var payload = asteroid.GetPayload();
 
-        foreach(KeyValuePair<GameObject, int> spawn in payload)
+        StartCoroutine("Spawn", payload);
+
+        audio.Play();
+        Destroy(asteroid);
+        Destroy(asteroid.gameObject, 10f);
+    }
+
+   IEnumerator Spawn(Dictionary<GameObject, int> payload)
+    {
+        foreach (KeyValuePair<GameObject, int> spawn in payload)
         {
             for (var i = 0; i < spawn.Value; i++)
             {
                 var manager = Instantiate(spawn.Key, transform.position, Quaternion.identity).GetComponent<WaypointManager>();
 
-                foreach(var waypoint in cachedPaths.GetRandom())
+                foreach (var waypoint in cachedPaths.GetRandom())
                 {
                     manager.AddWayPoint(waypoint.gameObject);
                 }
 
                 manager.SetCurrentWaypoint(0);
+
+                yield return new WaitForSeconds(0.5f);
             }
         }
-
-        audio.Play();
-        Destroy(asteroid);
-        Destroy(asteroid.gameObject, 5f);
     }
 }
